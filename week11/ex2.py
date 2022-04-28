25bf7a2bcd666cf5affcb6c99f96f089087cad3f
Aleksandr <45975122+quhaaST@users.noreply.github.com>
2022-04-28 20:41:31 +0300

    Update ex2.py

--- week11/ex2.py
+++ week11/ex2.py
@@ -1,16 +1,19 @@
 from pymongo import MongoClient
 
 
-def query1(query_db):
+def first_query(query_db):
     res = query_db.restaurants.insert_one(
-        {"address": {"building": "1480", "coord": [-73.9557413, 40.7720266], "street": "2 Avenue", "zipcode": "10075"},
+        {"address": {"building": "1480", "coord": [-73.9557413, 40.7720266], 
+                     "street": "2 Avenue", "zipcode": "10075"},
          "borough": "Manhattan", "cuisine": "Italian",
-         "grades": [{"date": {"$date": {"$numberLong": "1412121600000"}}, "grade": "A", "score": 11}], "name": "Vella",
+         "grades": [{"date": {"$date": {"$numberLong": "1412121600000"}}, 
+                     "grade": "A", "score": 11}], 
+         "name": "Vella",
          "restaurant_id": "41704620"})
     print(res.inserted_id)
 
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://localhost:5050")
 db = client['test']
 
-query1(db)
+first_query(db)
